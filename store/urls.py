9c6de72587cff4e51a487b8656a2eba9c66c93cd
Newt20 <47146755+Newt20@users.noreply.github.com>
2020-09-16 20:11:04 +0600

    made search.html and modified store.html, urls,py and views.py files

--- store/urls.py
+++ store/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
 	path('update_item/', views.updateItem, name="update_item"),
 	path('process_order/', views.processOrder, name="process_order"),
     path('success/', views.success, name="success"),
+	path('search/', views.search, name="product_search")
 
 ]
